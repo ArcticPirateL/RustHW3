21490c3b7a596b54bf3f5ca3f6430825ec1090e5
ArcticPirateL <3210104927@zju.edu.cn>
2023-09-11 00:27:41 +0800

    final

--- test2/src/main.rs
+++ test2/src/main.rs
@@ -1,50 +1,58 @@
 use std::ops::Deref;
-use std::cell::RefCell;
+use std::ptr::NonNull;
 
 struct MyRc<T> {
-    value: T,
-    count: RefCell<usize>,
+    value: NonNull<T>,
+    count: NonNull<usize>,
 }
 
-impl<T: Clone> MyRc<T> {
-    fn new(value: T) -> Self {
+impl<T> MyRc<T> {
+    pub fn new(data: T) -> Self {
+        let value_box = Box::new(data);
+        let count_box = Box::new(1);
         MyRc {
-            value,
-            count: RefCell::new(1),
+            value: NonNull::new(Box::into_raw(value_box)).unwrap(),
+            count: NonNull::new(Box::into_raw(count_box)).unwrap(),
         }
     }
-
-    fn clone(&self) -> Self {
-        *self.count.borrow_mut() += 1;
+    pub fn clone(&mut self) -> Self {
+        unsafe {
+            *self.count.as_mut() += 1;
+        }
         MyRc {
-            value: self.value.clone(),
-            count: self.count.clone(),
+            value: self.value,
+            count: self.count,
         }
     }
     fn strong_count(&self) -> usize {
-        return *self.count.borrow();
+        unsafe {
+            *self.count.as_ref()
+        }
     }
 }
 impl<T> Deref for MyRc<T> {
     type Target = T;
     fn deref(&self) -> &T {
-        &self.value
+        unsafe { self.value.as_ref() }
     }
 }
 impl<T> Drop for MyRc<T> {
     fn drop(&mut self) {
-        let mut count = self.count.borrow_mut();
-        *count -= 1;
-        if *count == 0 {
-            drop(&self.value);
+        unsafe {
+            if *self.count.as_ref() > 1 {
+                *self.count.as_mut() -= 1;
+            } else {
+                let _ = Box::from_raw(self.count.as_ptr());
+            }
         }
     }
 }
-
+//测试
 fn main() {
-    let rc1 = MyRc::new(42);
-    println!("Count1: {}", MyRc::strong_count(&rc1));
-    let rc2 = MyRc::clone(&rc1);
-    println!("Count2: {}", MyRc::strong_count(&rc1));
-    println!("rc1: {}, rc2: {}", *rc1, *rc2);
+    let mut rc1 = MyRc::new(42);
+    println!("count1 = {}", MyRc::strong_count(&rc1));
+    let rc2 = MyRc::clone(&mut rc1);
+    println!("count2 = {}", MyRc::strong_count(&rc1));
+    println!("rc1 = {}", *rc1);
+    println!("rc2 = {}", *rc2);
 }
